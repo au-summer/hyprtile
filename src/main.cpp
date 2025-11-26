94dedb33a61089815ac5ea7ca1f8d3b9ba588316
Xiayang Jin <jinxiayang0422@gmail.com>
2025-11-25 19:56:46 -0500

    update: header hash calculation

--- src/main.cpp
+++ src/main.cpp
@@ -144,15 +144,19 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
 {
     PHANDLE = handle;
 
-    const std::string HASH = __hyprland_api_get_hash();
+    const std::string COMPOSITOR_HASH = __hyprland_api_get_hash();
+    const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
 
     // ALWAYS add this to your plugins. It will prevent random crashes coming from
     // mismatched header versions.
-    if (HASH != GIT_COMMIT_HASH)
+    if (COMPOSITOR_HASH != CLIENT_HASH)
     {
-        HyprlandAPI::addNotification(PHANDLE, "[hyprtile] Mismatched headers! Can't proceed.",
+        HyprlandAPI::addNotification(PHANDLE,
+                                     "[hyprtile] Mismatched headers! Can't proceed.\nCompositor Hash: " +
+                                         COMPOSITOR_HASH + "\nClient Hash: " + CLIENT_HASH,
                                      CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
-        throw std::runtime_error("[hyprtile] Version mismatch");
+        throw std::runtime_error("[hyprtile] Version mismatch\nCompositor Hash: " + COMPOSITOR_HASH +
+                                 "\nClient Hash: " + CLIENT_HASH);
     }
 
     // Function Hooks
